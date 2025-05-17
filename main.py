da876b9ba1cfb9643e067e0c43293b7a3204da2b
maria <mariasophyavianagoncalves531@gmail.com>
2025-05-17 15:22:50 -0300

    criamos a pagina de menu

---
+++ main.py
@@ -0,0 +1,24 @@
+ 
+def menu():
+ while(true):   
+    print("seja bem vindo(a) ao sistema de cadastro")
+    print("1.cadastrar um usuario")
+    print("2.listar missoes")
+    print("3.simular lancamentos")
+    print("4.sair")
+    opcao = input("escolha uma dessas opcoes")
+
+    if opcao == '1':
+        cadastrarMissao()
+    elif opcao =='2':
+        listarMissoes()
+    elif opcao =='3':
+        simularLancamento()
+    elif opcao =='4':
+         print("saindo....")
+         break
+    else:
+        print("missao encerrada")
+
+    
+menu()    
